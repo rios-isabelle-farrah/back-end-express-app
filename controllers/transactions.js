9136267db427ba42762cdd6b969ee6930160f311
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-06-21 19:35:12 -0400

    restore staged

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,17 +2,11 @@ const transactions = require("express").Router();
 const transactionsArray = require("../models/transaction");
 // /transactions/
 transactions.get("/", (req, res) => {
-
   res.json(transactionsArray);
-  console.log('get transactions')
+  console.log("get transactions");
 });
 
-// const path = 'http://localhost:3001      /transactions    ?arrayId=25'
-// GET /transactions ----- arrayId=25
-// GET /transactions/banana ----- arrayId=25
-
 transactions.get("/:transIdx", (req, res) => {
-  // console.transaction(req.params);
   const { transIdx } = req.params;
   const transaction = transactionsArray[transIdx];
   if (transaction) {
@@ -22,7 +16,6 @@ transactions.get("/:transIdx", (req, res) => {
   }
 });
 
-// POST = /transactions
 transactions.post("/", (req, res) => {
   const { body } = req;
   transactionsArray.push(body);
@@ -32,21 +25,25 @@ transactions.post("/", (req, res) => {
   // res.json(transactionsArray);
 });
 
-// PUT - update action - /transactions/:id - put has a body
 transactions.put("/:transIdx", (req, res) => {
   const { transIdx } = req.params;
   const { body } = req;
   transactionsArray[transIdx] = body;
   res.json(transactionsArray[transIdx]);
-}); 
+});
 
-// DELETE - destroy action - /transactions/:id
 transactions.delete("/:transIdx", (req, res) => {
-  const { transIdx } = req.params; 
+  const { transIdx } = req.params;
   const deletedTransaction = transactionsArray.splice(transIdx, 1);
   res.json(deletedTransaction[0]);
-})
+});
 
-module.exports = transactions; 
+module.exports = transactions;
 
-
+// This criterion is linked to a Learning OutcomeAPI - RESTful Routes
+// 5 RESTful fully functioning routes:
+// - index
+// - show
+// - create
+// - update
+// - delete
