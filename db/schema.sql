6137038a67523feb5498fae950d3808620959723
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2022-02-16 11:00:57 -0500

    days data

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,75 @@
- DROP TABLE IF EXISTS days;
-
+-- DROP TABLE IF EXISTS days;
 
 -- \c portal;
+
 
 CREATE TABLE days (
     id SERIAL PRIMARY KEY, 
     name TEXT
 );
+
+
+
+-- DROP DATABASE IF EXISTS mileage_db;
+
+-- CREATE DATABASE mileage_db;
+
+-- \c mileage_db;
+
+
+-- DROP TABLE IF EXISTS mileage_db;
+-- CREATE TABLE
+--     cars(
+--         id SERIAL PRIMARY key,
+--         make TEXT not null,
+--         model TEXT not null,
+--         vin TEXT not null,
+--         year INT not null,
+--         odometer INT not null, 
+--         doors INT not null,
+--         is_default BOOLEAN NOT NULL,
+--         uid TEXT not null,
+--         driver TEXT not null
+--     );
+
+--  CREATE TABLE
+--     expenses(
+--         id SERIAL PRIMARY key,
+--         expense_type TEXT NOT NULL,
+--         business_use BOOLEAN NOT NULL,
+--         car_id INT REFERENCES cars (id) ON DELETE CASCADE,
+--         amount_spent TEXT NOT NULL,
+--         date TEXT 
+--     );
+
+-- CREATE TABLE
+--     trips(
+--         id SERIAL PRIMARY key,
+--         car_id INT REFERENCES cars (id) ON DELETE CASCADE,
+--         business_use BOOLEAN not null,
+--         miles INT NOT null,
+--         date TEXT,
+--         reason TEXT not null,	
+--         favorite BOOLEAN NOT NULL
+--     );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        --  start_odometer INT NOT null,
+        -- stop_odometer INT NOT null,
